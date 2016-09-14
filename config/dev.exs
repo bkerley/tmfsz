5c700cbc3b98dd97753ec88e97c5174982df55a5
Bryce Kerley <bkerley@brycekerley.net>
2016-09-14 16:30:02 -0400

    twitter

--- config/dev.exs
+++ config/dev.exs
@@ -38,3 +38,10 @@ config :tmfsz, Tmfsz.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "tmfsz_dev",
   pool_size: 10
+
+config :extwitter, :oauth, [
+    consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+    consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+    access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+    access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
+  ]
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Tmfsz.Mixfile do
   def application do
     [mod: {Tmfsz, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :extwitter]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,10 @@ defmodule Tmfsz.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:extwitter, "~> 0.7.2"},
+     {:oauth, github: "tim/erlang-oauth"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
