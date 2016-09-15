48175500ee576111d7cb26e3030c76785d71abee
Bryce Kerley <bkerley@brycekerley.net>
2016-09-14 21:59:08 -0400

    procfile

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config(:tmfsz, Tmfsz.Endpoint,
        url: [scheme: "https",
              host: "tmfsz.herokuapp.com",
              port: 443],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]]
+       force_ssl: [rewrite_on: [:x_forwarded_proto]],
        cache_static_manifest: "priv/static/manifest.json",
        secret_key_base: System.get_env("SECRET_KEY_BASE"))
 
