f3d0ebb325973c94ce3cec8d1cd414c1dd02206f
Bryce Kerley <bkerley@brycekerley.net>
2016-10-14 23:38:00 -0400

    prod url config

--- config/prod.exs
+++ config/prod.exs
@@ -17,10 +17,10 @@ config :logger, level: :info
 
 config(:tmfsz, Tmfsz.Endpoint,
        http: [port: {:system, "PORT"}],
-       # url: [scheme: "https",
-       #       host: "dasearch.zone",
-       #       port: 443],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+       url: [scheme: "https",
+             host: "dasearch.zone",
+             port: 443],
+       # force_ssl: [rewrite_on: [:x_forwarded_proto]],
        cache_static_manifest: "priv/static/manifest.json",
        secret_key_base: System.get_env("SECRET_KEY_BASE"))
 
